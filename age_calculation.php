2d491cd6e7363fb1b515c5e327622533e018e083
unknown <manjurulus@gmail.comm>
2024-07-02 09:50:30 +0600

    added age claculation

---
+++ age_calculation.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Age Calculation</title>
+</head>
+<body>
+    <form action="" method="post">
+        <label for="">Date of Birth</label>
+        <input type="date" name="dob">
+        <label for="">Current Date</label>
+        <input type="date" name="current">
+        <input type="submit" name="submit" value="Calculate">
+    </form>
+</body>
+</html>
